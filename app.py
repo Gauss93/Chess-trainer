c4d744d3a240e7ae4e0a3ffeb773266d201db20d
VotreNom <alexandredasilva1ga@gmail.com>
2026-02-22 15:43:13 +0100

    route + FEN initial

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
+from chess_logic import create_board, get_fen
 
 app = Flask(__name__)
+app.secret_key = "dev-secret-key" # Pour session !!!
 
 @app.route('/')
 def welcome():
     return render_template('welcome.html')
+
+@app.route('/new-game')
+def new_game():
+    board = create_board()
+    session["fen"] = get_fen(board)
+    return f"Nouvelle partie créée.<br>FEN : {session["fen"]}"
+
 
